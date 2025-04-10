e873c78cf9729762cb5d8c5a1c68498323ccc76d
parth <parthshivhare3@gmail.com>
2025-04-10 23:05:17 +0530

    Initial commit

---
+++ 02_new.js
@@ -0,0 +1,10 @@
+let score = "parth";
+// // const {score} = req.body
+// console.log(typeof score);
+// // console.log("Script is running");
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+let SomeNumber = 123;
+let StringNumber = String(SomeNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
